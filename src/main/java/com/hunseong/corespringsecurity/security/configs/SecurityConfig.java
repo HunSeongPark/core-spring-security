ec03c61ca285c7d319d4955247224d3aa7dacf5b
Hunseong <phunseong@gmail.com>
2022-06-05 18:12:51 +0900

    /users url permitAll

--- src/main/java/com/hunseong/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/com/hunseong/corespringsecurity/security/configs/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/", "/users").permitAll()
                 .antMatchers("/mypage").hasRole("USER")
                 .antMatchers("/messages").hasRole("MANAGER")
                 .antMatchers("/config").hasRole("ADMIN")
